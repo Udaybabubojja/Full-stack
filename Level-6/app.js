6c8fc608500128f188eedc53e402179434831f8e
UDAY-518 <111357317+Udaybabubojja@users.noreply.github.com>
2024-01-24 10:26:12 +0530

    Add files via upload

---
+++ Level-6/app.js
@@ -0,0 +1,68 @@
+/* eslint-disable indent */
+/* eslint-disable semi */
+/* eslint-disable quotes */
+const express = require("express");
+const app = express();
+const { Todo } = require("./models");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+app.get("/todos", async (request, response) => {
+    try {
+        const todos = await Todo.findAll();
+        return response.json(todos);
+    } catch (error) {
+        console.error(error);
+        return response.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.post("/todos", async (request, response) => {
+  console.log("post method..");
+  try {
+    const todo = await Todo.create({
+      title: request.body.title,
+      dueDate: request.body.dueDate,
+      completed: false
+    });
+    return response.json(todo);
+  } catch (error) {
+    console.error(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.put("/todos/:id/markAsCompleted", async (request, response) => {
+  console.log("we have id: ", request.params.id);
+  try {
+    const todo = await Todo.findByPk(request.params.id);
+
+    if (!todo) {
+      return response.status(404).json({ error: "Todo not found" });
+    }
+    await todo.markAsCompleted();
+    return response.json(todo);
+  } catch (error) {
+    console.error(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.delete("/todos/:id", async (request, response) => {
+    console.log("Deleted id: ", request.params.id);
+    try {
+        const todo = await Todo.findByPk(request.params.id);
+
+        if (!todo) {
+            return response.status(404).json({ error: 'Todo not found' });
+        }
+
+        await todo.destroy();
+        return response.json({ success: true });
+    } catch (error) {
+        console.error(error);
+        return response.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+module.exports = app;
